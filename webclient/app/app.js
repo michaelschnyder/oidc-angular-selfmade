e1f5e8b9b951696efa76e61b379ba957e39aa806
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-11-17 12:20:07 +0100

    create login link and add navigate to link no login click

--- webclient/app/app.js
+++ webclient/app/app.js
@@ -14,22 +14,44 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
 
 
   $routeProvider.otherwise({redirectTo: '/home'});
+  
 }]).
 
 run(['$rootScope', '$window', function($rootScope, $window) {
   
   // Some OAuth config
-  var tokenIssuerUri = 'http://';
-  var clientId = ''; 
-  var redirectUri = '';
-  var requestScopes = '';
+  var tokenIssuerUri = 'http://identity.amazing.ctd/connect/authorize';
+  var responseType = 'id_token';
+  var requestScopes = 'openid profile';
+
+  // About Me
+  var clientId = 'myAppId1234567890'; 
+  var redirectUri = 'http://webapp.amazing.ctd:8000/#/callback/login';
 
   $rootScope.login = function() {
 
+    var url = tokenIssuerUri
+
+              + "?response_type="
+              + encodeURIComponent(responseType)
+              
+              + "&client_id=" 
+              + encodeURIComponent(clientId) 
+              
+              + "&redirect_uri=" 
+              + encodeURIComponent(redirectUri) 
+              
+              + "&scope=" 
+              + encodeURIComponent(requestScopes)
+              
+              + "&nonce=" 
+              + encodeURIComponent('RandomNonce')
+              
+              + "&state=" 
+              + encodeURIComponent('RandomState')
+              ; 
+
+    $window.location.href = url;
   };
-
-  $rootScope.logout = function() {
-
-  }
 }])
 ;
